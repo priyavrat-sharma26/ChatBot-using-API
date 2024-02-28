40a57163917bb5fdb11b19cdacc7ffbae01a27fd
priyavrat-sharma26 <160008628+priyavrat-sharma26@users.noreply.github.com>
2024-02-29 02:40:34 +0530

    Add files via upload

---
+++ Sentiment_analysis.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+from transformers import pipeline
+class SentimentAnalysis():
+    def __init__(self,model_name="j-hartmann/emotion-english-distilroberta-base"):
+        self.classifier_roberta = pipeline("text-classification", model=model_name)
+    def give_sentimwnt(self,message):
+        return (list(self.classifier_roberta(message)[0].values())[0])
+
