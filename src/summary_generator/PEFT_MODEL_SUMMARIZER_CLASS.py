a1c67dde0c8f5327dd03f6c3426025c6eb2b3e76
saketsingh <saketsingh1@mphasis.com>
2024-03-11 16:26:59 +0530

    debugged some errors in peft model

--- src/summary_generator/PEFT_MODEL_SUMMARIZER_CLASS.py
+++ src/summary_generator/PEFT_MODEL_SUMMARIZER_CLASS.py
@@ -13,9 +13,11 @@ class DialogueSummarizer:
         self.model.eval()
 
     def generate_summary(self, dialogue):
-        inputs = self.tokenizer.encode("summarize: " + dialogue, return_tensors="pt", max_length=512, truncation=True)
+        text = " ".join(dialogue)
+        inputs = self.tokenizer.encode("summarize: " + text, return_tensors="pt", max_length=512, truncation=True)
         inputs = inputs.to(self.device)
         with torch.no_grad():
             summary_ids = self.model.generate(inputs, num_beams=4, max_length=150, early_stopping=True)
         summary = self.tokenizer.decode(summary_ids[0], skip_special_tokens=True)
         return summary
+
