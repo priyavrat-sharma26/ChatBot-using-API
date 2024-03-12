c0bf39286fffd97a30602bb6fa21418ee4efc6a0
nejiahmed777 <160204443+nejiahmed777@users.noreply.github.com>
2024-03-12 16:43:33 +0530

    Update sentiment analysis_model.py

--- src/sentiment_analysis/sentiment analysis_model.py
+++ src/sentiment_analysis/sentiment analysis_model.py
@@ -1,13 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[7]:
-
-
+# Importing the pipeline class from the transformers library
 from transformers import pipeline
-class SentimentAnalysis():
-    def __init__(self,model_name="j-hartmann/emotion-english-distilroberta-base"):
-        self.classifier_roberta = pipeline("text-classification", model=model_name)
-    def give_sentiment(self,message):
-        return (list(self.classifier_roberta(message)[0].values())[0])
 
+# Defining a class for sentiment analysis
+class SentimentAnalysis():
+    # Constructor to initialize the sentiment analysis pipeline
+    def __init__(self, model_name="j-hartmann/emotion-english-distilroberta-base"):
+        # Creating a pipeline for text classification using the specified model
+        self.classifier_roberta = pipeline("text-classification", model=model_name)
+    
+    # Method to perform sentiment analysis on a given message
+    def give_sentiment(self, message):
+        # Calling the pipeline to classify the sentiment of the message
+        # Extracting the sentiment label from the classification result
+        return list(self.classifier_roberta(message)[0].values())[0]
