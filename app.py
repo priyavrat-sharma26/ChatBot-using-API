2cea74a61f4b812008b959c324f7d9564d1468a5
Yogesh Jadhav <yogesh.jadhav1@mphasis.com>
2024-03-18 16:55:31 +0530

    app code refactoring WIP

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request,render_template
 from src.conversation.conversation_api import conversation_bp
 from src.sentiment_analysis.sentiment_analysis_api import sentiment_bp
 from src.suggestion_prediction.suggestion_api import suggestion_bp
@@ -14,6 +14,11 @@ app.register_blueprint(conversation_bp, url_prefix='/api/conversation')
 app.register_blueprint(sentiment_bp, url_prefix='/api/sentiment')
 app.register_blueprint(suggestion_bp, url_prefix='/api/suggestion')
 app.register_blueprint(summary_bp, url_prefix='/api/summary')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+ 
 
 if __name__ == '__main__':
     app.run(debug=True)
