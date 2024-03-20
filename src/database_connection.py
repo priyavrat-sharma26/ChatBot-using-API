f65c2d52dfd0339d0fd84fd544fc7ea3680a80f3
saketsingh <saketsingh1@mphasis.com>
2024-03-20 10:02:32 +0530

    database connection added

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,7 +1,12 @@
 
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
-from pymongo import MongoClient
+uri = "mongodb+srv://saket07singh:Fetjmi1920@clustersaket.80ewjt5.mongodb.net/?retryWrites=true&w=majority&appName=Clustersaket"
 
-client = MongoClient('mongodb://localhost:27017/')
+
+#client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient(uri, server_api=ServerApi('1'))
+
 db = client['conversation_db']
 collection = db['samples']
