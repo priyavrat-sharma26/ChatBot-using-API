58d8aef8c761137d774191f1a06ce432bea8e5fb
priyavrat-sharma26 <160008628+priyavrat-sharma26@users.noreply.github.com>
2024-04-11 10:25:40 +0530

    Update application.py

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ from src.conversation.conversation_api import conversation_bp
 from src.sentiment_analysis.sentiment_analysis_api import sentiment_bp
 from src.suggestion_prediction.suggestion_api import suggestion_bp
 from src.summary_generator.summary_generator_api import summary_bp
+from src.next_sentence_prediction_gpt2.gpt2_nsp_model_api import suggestion_gpt2_bp
 
 
 
@@ -14,6 +15,7 @@ app.register_blueprint(conversation_bp, url_prefix='/api/conversation')
 app.register_blueprint(sentiment_bp, url_prefix='/api/sentiment')
 app.register_blueprint(suggestion_bp, url_prefix='/api/suggestion')
 app.register_blueprint(summary_bp, url_prefix='/api/summary')
+app.register_blueprint(suggestion_gpt2_bp, url_prefix='/api/next_sentence')
 
 @app.route('/')
 def index():
